92e4ad0ea002717bc592bfed352f83feab24e8ed
alexandr polotovsky <apolotovsky@gmail.com>
2022-10-19 01:56:45 +0300

    add task_4

---
+++ task_4_Car.py
@@ -0,0 +1,67 @@
+"""
+Задание 4.
+Реализуйте базовый класс Car. У данного класса должны быть следующие публичные атрибуты:
+speed, color, name, is_police (булево).
+А также публичные методы: go, stop, turn(direction),
+которые должны сообщать, что машина поехала, остановилась, повернула (куда).
+Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar.
+Добавьте в базовый класс публичный метод show_speed,
+который должен показывать текущую скорость автомобиля.
+Для классов TownCar и WorkCar переопределите метод show_speed.
+При значении скорости свыше 60 (TownCar)
+и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
+Создайте экземпляры классов, передайте значения атрибутов.
+Выполните доступ к атрибутам, выведите результат.
+Выполните вызов методов и также покажите результат.
+"""
+
+
+class Car:
+
+    def __init__(self, speed, color, name, is_police: bool):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print(f'Машина {self.name} поехала.')
+
+    def stop(self):
+        self.speed = 0
+        print('Машина остановилась.')
+
+    def turn(self, direction):
+        print(f'Машина {self.name} повернула {direction}.')
+
+    def show_speed(self):
+        print(f'скорость: {self.speed}')
+
+
+class TownCar(Car):
+
+    def show_speed(self):
+        if self.speed > 60:
+            print('Вы превысили скорость!')
+        print(f'скорость: {self.speed}')
+
+
+class SportCar(Car):
+
+    def answer(self):
+        self.speed = 200
+        print('А ты догони...')
+
+
+class WorkCar(Car):
+
+    def show_speed(self):
+        if self.speed > 40:
+            print('Вы превысили скорость!')
+        print(f'скорость: {self.speed}')
+
+
+class PoliceCar(Car):
+
+    def megafon(self, obj_name):
+        print(f'Автомобиль {obj_name} прижмитесь к обочине.')
